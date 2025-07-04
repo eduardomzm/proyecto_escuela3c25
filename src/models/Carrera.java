ce8d6032cd7a677aece3b4260be3b9e450dbb507
eduardomzm <eduardo.mezam@outlook.com>
2025-07-04 14:02:02 -0600

    JInternalFrame Insertar Carrera

---
+++ src/models/Carrera.java
@@ -0,0 +1,28 @@
+package models;
+
+public class Carrera {
+    private int idCarrera;
+    private String nombre;
+    private double monto;
+    public int getIdCarrera() {
+        return idCarrera;
+    }
+    public void setIdCarrera(int idCarrera) {
+        this.idCarrera = idCarrera;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public double getMonto() {
+        return monto;
+    }
+    public void setMonto(double monto) {
+        this.monto = monto;
+    }
+
+    
+    
+}
--- src/screens/VentanaPrincipal.java
+++ src/screens/VentanaPrincipal.java
@@ -10,25 +10,63 @@ public class VentanaPrincipal extends JFrame{
     public JDesktopPane desktop; // Escritorio dentro del Frame
     private JMenuBar jMenuBar;
     private JMenu jMenuAlumnos;
+    private JMenu jMenuCarreras;
     private JMenuItem jMenuItemInsertar;
+    private JMenuItem jMenuItemVerAlumnos;
+    private JMenuItem jMenuItemInsertarCarrera;
+    private JMenuItem JMenuItemVerCarreras;
+
  
-    public VentanaPrincipal(){
+    public VentanaPrincipal(String tittle){
+        this.setTitle(tittle);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         initComponents();
     }
 
     private void initComponents(){
+        // DesktopPane - escritorio
         desktop = new JDesktopPane();
         this.add(desktop);
+        // Barra de Menu
         jMenuBar = new JMenuBar();
         this.add(jMenuBar);
+        // Menu alumnos
         jMenuAlumnos = new JMenu();
         jMenuAlumnos.setText("Alumnos");
+        // Elemento de menu: Insertar
         jMenuItemInsertar = new JMenuItem();
         jMenuItemInsertar.setText("Insertar");
+        // Elemento de menu: Ver alumnos
+        jMenuItemVerAlumnos = new JMenuItem();
+        jMenuItemVerAlumnos.setText("Ver todos los alumnos");
+
+        // Menu carreras
+        jMenuCarreras = new JMenu();
+        jMenuCarreras.setText("Carreras");
+        // Elemento de menu: Carreras
+        JMenuItemVerCarreras = new JMenuItem();
+        JMenuItemVerCarreras.setText("Ver todas las carreras");
+
+        jMenuItemInsertarCarrera = new JMenuItem();
+        jMenuItemInsertarCarrera.setText("Insertar");
+        jMenuItemInsertarCarrera.addActionListener(e -> {
+            JInternalFrameInsertarCarrera insertarCarrera = new JInternalFrameInsertarCarrera();
+            insertarCarrera.setVisible(true);
+            this.desktop.add(insertarCarrera);
+        });
+
+
+        // Agregar menuItema menu
+        jMenuAlumnos.add(jMenuItemVerAlumnos);
         jMenuAlumnos.add(jMenuItemInsertar);
         jMenuBar.add(jMenuAlumnos);
+
+        jMenuCarreras.add(JMenuItemVerCarreras);
+        jMenuCarreras.add(jMenuItemInsertarCarrera);
+        jMenuBar.add(jMenuCarreras);
+
+
         this.setJMenuBar(jMenuBar);
         pack();
-    
     }
 }
